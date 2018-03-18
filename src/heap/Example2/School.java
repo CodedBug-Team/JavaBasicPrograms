ba800632e1a34f73e1a56a7c984bc45cf8a9e99f
Abhishek <Abhishek@192.168.15.66>
2018-03-18 18:35:05 +0530

    Minor changes

--- src/heap/Example2/School.java
+++ src/heap/Example2/School.java
@@ -2,23 +2,3 @@ package heap.Example2;
 
 class School {
 }
-
-class Student {
-School n;               // instance variable
-String Lname;           // instance variable
-
-public static void main(String args[]){
-Student s;                               //local variable:s
-s = new Student();
-s.subjects(s);
-}
-
-void subjects(Student stud){             //local variable: stud
-n = new School();
-stud.setName("Alice");
-}
-
-void setName(String studLastName){       //local variable: studName 
-Lname = studLastName;
-}
-}
